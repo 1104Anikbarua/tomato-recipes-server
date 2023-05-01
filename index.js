6383aae9e72fa809759957c0722170748140ff8e
Anik Barua <anikbb04@gmail.com>
2023-05-02 03:31:55 +0600

    create routes for load chef data

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+app.use(cors());
+
+const chefs = require('./data/chef.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
